da87d5a43d8216a08841b25b46472d30b9c912c7
larisaa07 <larisa_aveti@yahoo.com>
2025-05-07 13:54:28 -0400

    literally no progress...

--- script.js
+++ script.js
@@ -17,3 +17,88 @@ const vue_app = Vue.createApp({
   });
   
   vue_app.mount("#vue_app");
+
+//testimonial page
+const teamMembers = [
+  {
+      name: 'Zeus',
+      comment: 'what they said'
+  },
+  {
+      name: 'Zeus',
+      comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  },
+  {
+    name: 'Zeus',
+    comment: 'what they said'
+  }
+]
+
+function generateTeamCards(){
+  const teamCardsContainer = document.getElementById('teamCards');
+
+  teamMembers.forEach(member => {
+      const card = document.createElement('div')
+      card.classList.add('col-md-4')
+
+       //Create a list of titleDomain with <li> tags
+  const titleDomainList = member.titleDomain
+  .map(titleDomain => `<li> ${titleDomain} </li>`).join('')
+
+      //Create a list of symbols with <li> tags
+  const symbolsList = member.symbols
+  .map(symbols => `<li> ${symbols} </li>`).join('')
+
+      card.innerHTML=`
+          <div class="card">
+              <div   class = "card-header">
+                  ${member.name}
+              </div>
+              <div class="card-body" style = "background-color:${backgroundColor};">
+                  <p><strong>Comments:</strong> ${member.comment}</p>
+              </div>
+          </div>
+      `
+
+          
+  teamCardsContainer.appendChild(card)
+  });
+}
+
+window.onload = generateTeamCards()
